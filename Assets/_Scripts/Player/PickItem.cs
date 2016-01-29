ef193ee375e526f21485edeb33868f2d6f794318
TuomoO <tuomooksanen@kamk.fi>
2016-01-29 22:20:03 +0200

    Added script for picking items

---
+++ Assets/_Scripts/Player/PickItem.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickItem : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0.5f));
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.DrawRay(hit.point, hit.normal, Color.red, 5.0f);
+                if (hit.transform.gameObject.tag == "PickUp")
+                    Debug.Log("picked up");
+            }
+        }
+
+	}
+}
---
+++ Assets/_Scripts/Player/PlayerInput.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInput : MonoBehaviour 
+{
+    public float walkSpeed;
+    public float backStepSpeed;
+
+    private Vector3 mousePos = new Vector3(255, 255, 255);
+    private float mouseSens = 0.25f;
+
+	// Use this for initialization
+	void Start () 
+    {
+        walkSpeed = 5.0f;
+        backStepSpeed = 2.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        mousePos = Input.mousePosition - mousePos;
+        mousePos = new Vector3(-mousePos.y * mouseSens, mousePos.x * mouseSens, 0);
+        mousePos = new Vector3(transform.eulerAngles.x + mousePos.x, transform.eulerAngles.y + mousePos.y, 0);
+        transform.eulerAngles = mousePos;
+        mousePos = Input.mousePosition;
+        
+        if (Mathf.Abs(x) > 0)
+        {
+            Vector3 movement = transform.right;
+            movement *= x * walkSpeed;
+            transform.position += movement * Time.deltaTime;
+        }
+        if (Mathf.Abs(y) > 0)
+        {
+            Vector3 movement = transform.forward;
+            movement *= y;
+            y *= y > 0 ? walkSpeed : backStepSpeed;  
+            transform.position += movement * Time.deltaTime;
+        }
+        transform.position = new Vector3(transform.position.x, 4, transform.position.z);
+	}
+}
