b067e8d2f3685871a063e224e4c559a50be30f60
havyton <tti13smiikas@kamk.fi>
2016-01-30 12:59:01 +0200

    Added some main menu textures (placeholder) and added player sight mesh (for mesh collider)

--- Assets/_Scripts/AI/AI.cs
+++ Assets/_Scripts/AI/AI.cs
@@ -55,6 +55,7 @@ public class AI : MonoBehaviour {
                     }
                     break;
                 }
+
         }
 
 	}
---
+++ Assets/_Scripts/MainMenuScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void StartGame()
+    {
+        Application.LoadLevel("game");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
