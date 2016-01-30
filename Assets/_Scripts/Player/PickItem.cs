04c4d16bf8849943dc3158685860202598d80231
TuomoO <tuomooksanen@kamk.fi>
2016-01-30 13:10:34 +0200

    Added ingredient spawning

--- Assets/_Scripts/Player/PickItem.cs
+++ Assets/_Scripts/Player/PickItem.cs
@@ -4,20 +4,26 @@ using System.Collections;
 public class PickItem : MonoBehaviour 
 {
 
-    private GameObject heldItem = null;
+    private GameObject heldItem;
+    private float maxGrabDistance;
+    private Vector2 holdDistance;
+    private float throwForce;
 
 	// Use this for initialization
 	void Start () 
     {
         heldItem = null;
+        maxGrabDistance = 5.0f;
+        holdDistance = new Vector2(2.5f, 0.75f);
+        throwForce = 7.5f;
 	}
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (heldItem == null)
         {
-            if (heldItem == null)
+            if (Input.GetMouseButtonDown(0))
             {
                 Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0.5f));
                 RaycastHit hit;
@@ -26,13 +32,21 @@ public class PickItem : MonoBehaviour
                     Debug.DrawRay(hit.point, hit.normal, Color.red, 5.0f);
                     if (hit.transform.gameObject.tag == "PickUp")
                     {
-                        heldItem = hit.transform.gameObject;
-                        PickObject();
-                        Debug.Log("picked up");
+                        if (hit.distance < maxGrabDistance)
+                        {
+                            heldItem = hit.transform.gameObject;
+                            PickObject();
+                            Debug.Log("picked up");
+                        }
+                        else
+                            Debug.Log("too far to grab!");
                     }
                 }
             }
-            else
+        }
+        else
+        {
+            if (Input.GetMouseButtonDown(0))
                 DropObject();
         }
     }
@@ -40,7 +54,7 @@ public class PickItem : MonoBehaviour
     private void PickObject()
     {
         heldItem.transform.parent = this.transform;
-        heldItem.transform.position += transform.forward * 0.5f + transform.up * 1.25f;
+        heldItem.transform.position = transform.position + transform.forward * holdDistance.x + transform.up * holdDistance.y;
         heldItem.GetComponent<Rigidbody>().isKinematic = true;
     }
 
@@ -48,7 +62,10 @@ public class PickItem : MonoBehaviour
     private void DropObject()
     {
         heldItem.transform.parent = null;
-        heldItem.GetComponent<Rigidbody>().isKinematic = false;
+        Rigidbody body = heldItem.GetComponent<Rigidbody>();
+        body.isKinematic = false;
+        body.AddForce(throwForce * transform.forward + throwForce * 0.5f * transform.up, ForceMode.Impulse);
+        body.AddForceAtPosition(0.1f * throwForce * transform.forward, new Vector3(0, 0, 1), ForceMode.Impulse);
         heldItem = null;
     }
 }
---
+++ Assets/_Scripts/Recipe/SpawnIngredients.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnIngredients : MonoBehaviour {
+
+    public GameObject pickUp;
+    private const int INGREDIENT_TYPES = 20;
+    private const int SPAWN_COUNT = 5;
+
+	// Use this for initialization
+	void Start () 
+    {
+        Debug.Log("spawn start");
+        for (int i = 0; i < SPAWN_COUNT; i++)
+        {
+            Debug.Log("spawn");
+            GameObject obj1 = transform.GetChild((Random.Range(0, (int)transform.childCount) - 1)).gameObject;
+            GameObject obj2 = pickUp;//(GameObject)Resources.Load("_Prefabs/PickUp");
+            obj2.transform.position = obj1.transform.position;
+            Destroy(obj1);
+            Instantiate(obj2);
+        }
+	}
+
+    // Update is called once per frame
+    void Update() 
+    {
+	
+	}
+}
