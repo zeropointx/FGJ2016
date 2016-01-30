185ab218f45db77f4a6db6c6f80259a519458c5d
TuomoO <tuomooksanen@kamk.fi>
2016-01-30 21:35:53 +0200

    Model swapping for pickups should work now

--- Assets/_Scripts/Ingredient.cs
+++ Assets/_Scripts/Ingredient.cs
@@ -8,13 +8,14 @@ public class Ingredient : MonoBehaviour
     public Vector3 startPos;
 
     public GameObject burger;
+    public GameObject pepsi;
 
     public enum Type
     {
         A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U
     };
 
-    private Type type;
+    private Type type = Type.A;
     public bool isBeingPickedUp = false;
     
     // Use this for initialization
@@ -38,14 +39,44 @@ public class Ingredient : MonoBehaviour
     public void SetType(Type _type)
     {
         type = _type;
-        /*
-        GameObject old = transform.parent.GetChild(0).gameObject;
-        old.SetActive(false);
-        GameObject newModel = Instantiate(burger);
+
+        GameObject newModel = null;
+        switch(type)
+        {
+            case Type.A:
+            case Type.B:
+            case Type.C:
+            case Type.D:
+            case Type.E:
+            case Type.F:
+            case Type.G:
+            case Type.H:
+            case Type.I:
+            case Type.J:
+                newModel = Instantiate(pepsi);
+                break;
+
+            case Type.K:
+            case Type.M:
+            case Type.N:
+            case Type.O:
+            case Type.P:
+            case Type.Q:
+            case Type.R:
+            case Type.S:
+            case Type.T:
+            case Type.U:
+                newModel = Instantiate(burger);
+                break;
+
+            default:
+                Debug.Log("Invalid ingredient type!");
+                return;
+        }
+        GameObject old = transform.GetChild(0).gameObject;
         newModel.transform.position = old.transform.position;
         newModel.transform.rotation = old.transform.rotation;
-        Destroy(old);
+        DestroyImmediate(old);
         newModel.transform.parent = transform;
-        */
     }
 }
