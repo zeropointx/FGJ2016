67081c72e0a6f49f63257573a551697f37685859
Zegram <Joikkiz@hotmail.com>
2016-01-31 13:11:53 +0200

    HUD

---
+++ Assets/_Scripts/HUD.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour {
+
+    GameObject text;
+    GameObject cauldron;
+    CauldronScript cauldronScript;
+	// Use this for initialization
+	void Start () 
+    {
+        text = GameObject.Find("ItemsLeft");
+        cauldron = GameObject.Find("Pata");
+        cauldronScript = cauldron.GetComponent<CauldronScript>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	text.GetComponent<Text>().text = "Items placed: " + cauldronScript.currentAmount + " / " + cauldronScript.winAmount;
+	}
+}
